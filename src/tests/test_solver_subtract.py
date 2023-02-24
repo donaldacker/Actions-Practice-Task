b59b079b2fc6047b8cc06cc2c38af4ef730d6256
Donald Acker <99315935+donaldacker@users.noreply.github.com>
2023-02-24 18:24:44 -0500

    Update and rename test_solver_substract.py to src/tests/test_solver_subtract.py

--- src/tests/test_solver_subtract.py
+++ src/tests/test_solver_subtract.py
@@ -10,19 +10,19 @@ import os
 THIS_DIR = os.path.dirname(__file__)
 SCRIPT_LOCATION = os.path.dirname(THIS_DIR)
 sys.path.append(SCRIPT_LOCATION)
-import solver_substract
+import solver_subtract
 
 class TestSolver(unittest.TestCase):
     """ Test class for the dummy solver """
 
     def test_solve(self):
-        self.assertEqual(solver_substract.solve(1, 2), -1)
+        self.assertEqual(solver_subtract.solve(1, 2), -1)
 
     def test_solve_negative(self):
-        self.assertEqual(solver_substract.solve(-1, -2), 1)
+        self.assertEqual(solver_subtract.solve(-1, -2), 1)
 
     def test_solve_zero(self):
-        self.assertEqual(solver_substract.solve(0, 0), 0)
+        self.assertEqual(solver_subtract.solve(0, 0), 0)
 
 
 if __name__ == "__main__":
