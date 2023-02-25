5ca7da33e85a0887110fb73a6e7454cca00d9dfb
Donald Acker <99315935+donaldacker@users.noreply.github.com>
2023-02-25 08:19:03 -0500

    Update tester.py

--- src/tester.py
+++ src/tester.py
@@ -4,10 +4,11 @@ import xmlrunner
 
 THIS_DIR = os.path.dirname(__file__)
 TEST_LOCATION = THIS_DIR + '/tests'
+PARENT_DIR = os.path.dirname(THIS_DIR)
 
 suite = unittest.defaultTestLoader.discover(TEST_LOCATION)
 
 if __name__ == '__main__':
-    with open(THIS_DIR+'/results.xml', 'wb') as outfile:
+    with open(PARENT_DIR+'/results.xml', 'wb') as outfile:
         runner = xmlrunner.XMLTestRunner(output=outfile)
         runner.run(suite)
